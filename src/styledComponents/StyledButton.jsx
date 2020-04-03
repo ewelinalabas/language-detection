3441356224cb6b19d2de6945d5ed649dd2277132
Ewelina Łabas <ewelina.labas@gmail.com>
2020-04-03 14:42:04 +0200

    Fix styles for buttons

--- src/styledComponents/StyledButton.jsx
+++ src/styledComponents/StyledButton.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { Button } from '@material-ui/core';
 
 export const StyledButton = styled(Button)({
-  // margin: "0px 2.5px 0px 2.5px !important",
   minWidth: "40% !important",
   maxWidth: "40% !important",
   minHeight: "100% !important",
